b079dbc8522f3db134ce7221885e72ccd1c1d377
Lucas Myers <lucasmyers97@gmail.com>
2023-03-22 15:51:36 -0500

    Added phase_field_crystal_systems class

---
+++ src/phase_field_crystal_systems/phase_field_crystal_system.cpp
@@ -0,0 +1,9 @@
+#include "phase_field_crystal_system.hpp"
+
+template <int dim>
+PhaseFieldCrystalSystem<dim>::PhaseFieldCrystalSystem()
+{}
+
+
+template class PhaseFieldCrystalSystem<2>;
+template class PhaseFieldCrystalSystem<3>;
---
+++ src/phase_field_crystal_systems/phase_field_crystal_system.hpp
@@ -0,0 +1,11 @@
+#ifndef PHASE_FIELD_CRYSTAL_SYSTEM
+#define PHASE_FIELD_CRYSTAL_SYSTEM
+
+template <int dim>
+class PhaseFieldCrystalSystem
+{
+public:
+    PhaseFieldCrystalSystem();
+};
+
+#endif
