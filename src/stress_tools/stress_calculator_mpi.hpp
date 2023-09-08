5ff4250d7e291f7cf33ce360f4c9ce7747db2ff0
Lucas Myers <lucasmyers97@gmail.com>
2023-09-08 16:13:56 -0500

    Added documentation to StressCalculatorMPI class

--- src/stress_tools/stress_calculator_mpi.hpp
+++ src/stress_tools/stress_calculator_mpi.hpp
@@ -11,6 +11,36 @@
 #include <vector>
 #include <filesystem>
 
+/**
+ * \brief Calculates tensorial stress field associated with the phase field
+ *
+ * Given a triangulation and degree of Lagrange polynomials, this class
+ * creates all of the machinery necessary for calculating the stress tensor 
+ * field \f$\sigma\f$ arising from the phase-field \f$\Psi\f$.
+ * For sake of efficiency, this calculation is broken up into three steps
+ * which all have to be called initially in order to do the operation.
+ * These are:
+ *
+ * - setup_dofs
+ * - calculate_mass_matrix
+ * - calculate_stress
+ *
+ * After the initial calculation, only `calculate_stress` needs to be rerun
+ * on update of \f$\Psi\f$.
+ * setup_dofs needs to be rerun only if the Triangulation is modified, and 
+ * calculate_mass_matrix needs to be rerun only if setup_dofs is.
+ * Typical usage is:
+ * ```cpp
+ * StressCalculatorMPI stress_calculator(tria, degree);
+ *
+ * stress_calculator.setup_dofs(mpi_communicator);
+ * stress_calculator.calculate_mass_matrix();
+ * stress_calculator.calculate_stress(mpi_communicator,
+ *                                    Psi_dof_handler,
+ *                                    Psi,
+ *                                    eps);
+ * ```
+ */
 template <int dim>
 class StressCalculatorMPI
 {
